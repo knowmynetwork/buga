e1267cb9d946f47d96e7f93db661b2c2bfee3cc3
Adedayo Victor <Adedayovictor457@gmail.com>
2025-03-31 02:02:01 +0100

    updates and fixes

--- lib/driver/saved_places/saved_place.dart
+++ lib/driver/saved_places/saved_place.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_google_places_sdk/flutter_google_places_sdk.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uuid/uuid.dart';
 import '../../../../Provider/riders/saved_places.dart';
 import '../../../../service/service_export.dart';
 import '../../../../viewmodels/ridermodel/saved_place.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SavedPlaces extends ConsumerStatefulWidget {
   const SavedPlaces({super.key});
@@ -17,65 +19,77 @@ class _SavedPlacesState extends ConsumerState<SavedPlaces> {
   final TextEditingController _addressController = TextEditingController();
   final TextEditingController _infoController = TextEditingController();
 
-  late FlutterGooglePlacesSdk places;
-  List<AutocompletePrediction> predictions = [];
-
-  /// Fetch address suggestions from Google Places API
-  Future<void> fetchPredictions(String input) async {
-    if (input.isEmpty) return;
-
-    final result = await places.findAutocompletePredictions(
-      input,
-      countries: ["NG"], // Restrict search to a specific country (optional)
-    );
-
-    setState(() {
-      predictions = result.predictions;
-    });
-  }
-
-  Future<void> getPlaceDetails(String placeId) async {
-    final result = await places.fetchPlace(
-      placeId,
-      fields: [
-        PlaceField.Location,
-        PlaceField.Name,
-        PlaceField.Address,
-        PlaceField.Id,
-        PlaceField.PhoneNumber,
-        PlaceField.Rating,
-        PlaceField.Types,
-        PlaceField.PriceLevel,
-        PlaceField.AddressComponents,
-      ], // Fetch required fields
-    );
-
-    if (result.place?.latLng != null) {
-      final lat = result.place!.latLng!.lat;
-      final lng = result.place!.latLng!.lng;
-
-      print("🏠 Selected Place: ${result.place!.name}");
-      print("📍 Latitude: $lat, Longitude: $lng");
-
-      print('This is  the complete result$result ');
-    } else {
-      print("⚠️ Failed to fetch location details.");
-    }
-  }
+  var uuid = Uuid();
+  String? _sessionToken;
+  List<dynamic> _placeList = [];
 
   @override
   void initState() {
     super.initState();
-    places = FlutterGooglePlacesSdk(mapKey);
+    _addressController.addListener(() {
+      _onChanged();
+    });
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     _nameController.dispose();
     _addressController.dispose();
     _infoController.dispose();
+  }
+
+  _onChanged() {
+    if (_sessionToken == null) {
+      setState(() {
+        _sessionToken = uuid.v4();
+      });
+    }
+    getSuggestion(_addressController.text);
+  }
+
+  void getSuggestion(String input) async {
+    String kplacesApiKey = mapKey;
+    String baseURL =
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+    String request =
+        '$baseURL?input=$input&key=$kplacesApiKey&sessiontoken=$_sessionToken';
+    var response = await http.get(
+      Uri.parse(request),
+    );
+    if (response.statusCode == 200) {
+      setState(() {
+        _placeList = json.decode(response.body)['predictions'];
+      });
+    } else {
+      throw Exception('Failed to load predictions');
+    }
+  }
+
+  void _onSuggestionTap(Map<String, dynamic> place) async {
+    setState(() {
+      _addressController.text = place['description'];
+      _placeList = [];
+    });
+    print('Selected Place Details:');
+    print(json.encode(place));
+
+    // Fetch detailed place information
+    String placeId = place['place_id'];
+    String kplacesApiKey = mapKey;
+    String detailsUrl =
+        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$kplacesApiKey';
+
+    var response = await http.get(Uri.parse(detailsUrl));
+    if (response.statusCode == 200) {
+      var result = json.decode(response.body)['result'];
+      print('Detailed Place Information:');
+      print('Latitude: ${result['geometry']['location']['lat']}');
+      print('Longitude: ${result['geometry']['location']['lng']}');
+      print('others....: $result...');
+    } else {
+      print('Failed to fetch detailed place information');
+    }
   }
 
   @override
@@ -99,27 +113,22 @@ class _SavedPlacesState extends ConsumerState<SavedPlaces> {
             const SizedBox(height: 10),
             TextField(
               controller: _addressController,
-              onChanged: (String val) {
-                fetchPredictions(_addressController.text);
-              },
               decoration: const InputDecoration(
                 labelText: "Address",
                 border: OutlineInputBorder(),
               ),
             ),
-            ...predictions.map((prediction) {
-              return ListTile(
-                title: Text(prediction.fullText ?? ""),
-                onTap: () {
-                  _addressController.text = prediction.fullText;
-                  getPlaceDetails(prediction.placeId);
-
-                  // getPlaceDetails(prediction.placeId!);
-                  setState(() =>
-                      predictions.clear()); // Clear suggestions after selection
-                },
-              );
-            }),
+            ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: _placeList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_placeList[index]["description"]),
+                  onTap: () => _onSuggestionTap(_placeList[index]),
+                );
+              },
+            ),
             const SizedBox(height: 10),
             TextField(
               controller: _infoController,
@@ -164,107 +173,3 @@ class _SavedPlacesState extends ConsumerState<SavedPlaces> {
     );
   }
 }
-
-// class SavedPlaces extends StatefulWidget {
-//   const SavedPlaces({super.key});
-
-//   @override
-//   _SavedPlacesState createState() => _SavedPlacesState();
-// }
-
-// class _SavedPlacesState extends State<SavedPlaces> {
-//   final TextEditingController _nameController = TextEditingController();
-//   final TextEditingController _addressController = TextEditingController();
-//   final TextEditingController _infoController = TextEditingController();
-
-//   Future<void> _savePlace() async {
-//     String name = _nameController.text.trim();
-//     String address = _addressController.text.trim();
-//     String info = _infoController.text.trim();
-
-//     if (name.isEmpty || address.isEmpty) {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         const SnackBar(content: Text("Name and Address are required!")),
-//       );
-//       return;
-//     }
-
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     List<String> savedPlaces = prefs.getStringList('saved_places') ?? [];
-
-//     Map<String, String> newPlace = {
-//       "name": name,
-//       "address": address,
-//       "info": info,
-//     };
-
-//     savedPlaces.add(jsonEncode(newPlace));
-//     await prefs.setStringList('saved_places', savedPlaces);
-
-//     ScaffoldMessenger.of(context).showSnackBar(
-//       const SnackBar(content: Text("Place saved successfully!")),
-//     );
-
-//     _nameController.clear();
-//     _addressController.clear();
-//     _infoController.clear();
-//     Navigator.pop(context, true); // Return to list screen and refresh it
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Add Saved Place"),
-//         backgroundColor: Colors.yellow[700],
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           children: [
-//             TextField(
-//               controller: _nameController,
-//               decoration: const InputDecoration(
-//                 labelText: "Place Name",
-//                 border: OutlineInputBorder(),
-//               ),
-//             ),
-//             const SizedBox(height: 10),
-//             TextField(
-//               controller: _addressController,
-//               decoration: const InputDecoration(
-//                 labelText: "Address",
-//                 border: OutlineInputBorder(),
-//               ),
-//             ),
-//             const SizedBox(height: 10),
-//             TextField(
-//               controller: _infoController,
-//               decoration: const InputDecoration(
-//                 labelText: "Additional Info",
-//                 border: OutlineInputBorder(),
-//               ),
-//               maxLines: 3,
-//             ),
-//             const SizedBox(height: 20),
-//             SizedBox(
-//               width: double.infinity,
-//               child: ElevatedButton(
-//                 onPressed: _savePlace,
-//                 style: ElevatedButton.styleFrom(
-//                   padding: const EdgeInsets.symmetric(vertical: 14),
-//                   backgroundColor: Colors.yellow[700],
-//                 ),
-//                 child: const Text("Save Place", style: TextStyle(fontSize: 16)),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
- 
- 
- 
- 
-//   }
-// }
